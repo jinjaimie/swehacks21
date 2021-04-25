8c2ca70de02d7fff67c4ea450f4065fcaacee4cc
Jaimie Jin <jinjaimie12@gmail.com>
2021-04-24 20:10:44 -0700

    App + routing framework

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Route, Switch } from 'react-router';
 import './App.css';
 
 function App() {
   return (
     <div>
-      
+      <header>
+
+      </header>
+      <main>
+        <Switch>
+          <Route></Route>
+        </Switch>
+      </main>
+      <footer>
+
+      </footer>
     </div>
   );
 }
