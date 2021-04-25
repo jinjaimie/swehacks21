77a4e0296fd248ad990e1d81817187814dfde135
iheppe <iheppe@uw.edu>
2021-04-25 10:13:26 -0700

    added detail page

--- src/App.js
+++ src/App.js
@@ -3,26 +3,28 @@ import React, { useState } from 'react';
 import {Switch, Redirect, Route} from 'react-router-dom';
 import { useParams } from 'react-router';
 import {CardInfo} from './Card';
-import CardDeck from 'react-bootstrap/CardDeck'
-import choiceOptions from './choices.json'
+import {Details} from './Detail';
+import CardDeck from 'react-bootstrap/CardDeck';
+import choiceOptions from './choices.json';
 
 const FAKE_DATA = [
   {
     url: "black-lives-matter",
     name: "Black Lives Matter",
     image: "blm.jpg",
-    description: "",
+    description: "#BlackLivesMatter was founded in 2013 in response to the acquittal of Trayvon Martin’s murderer. Black Lives Matter Global Network Foundation, Inc. is a global organization in the US, UK, and Canada, whose mission is to eradicate white supremacy and build local power to intervene in violence inflicted on Black communities by the state and vigilantes. By combating and countering acts of violence, creating space for Black imagination and innovation, and centering Black joy, we are winning immediate improvements in our lives.",
   },
   {
     url: "stop-asian-hate",
     name: "Stop Asian Hate",
-    image: "sah.jpg"
+    image: "sah.jpg",
+    description: "In response to the alarming escalation in xenophobia and bigotry resulting from the COVID-19 pandemic, the Asian Pacific Planning and Policy Council (A3PCON), Chinese for Affirmative Action (CAA), and the Asian American Studies Department of San Francisco State University launched the Stop AAPI Hate reporting center on March 19, 2020. The center tracks and responds to incidents of hate, violence, harassment, discrimination, shunning, and child bullying against Asian Americans and Pacific Islanders in the United States."
   }
 ]
 
 function App() {
-  const [data, setData] = useState(FAKE_DATA)
-  const [choices, setChoices] = useState(choiceOptions)
+  const [data, setData] = useState(FAKE_DATA) // the user's personal interests
+  const [choices, setChoices] = useState(choiceOptions) // all available options
   return (
     <div>
       <header>
@@ -30,8 +32,9 @@ function App() {
       </header>
       <main>
         <Switch>
-          <Route path="/choices/:choice"><MoreInfo data={data}/></Route>
-          <Route path="/dashboard"><Dashboard data={choices} /></Route>
+          <Route exact path="/choices"><Dashboard data={choices} /></Route>
+          <Route path="/choices/:choice"><MoreInfo data={choices}/></Route>
+          <Route path="/dashboard"><Dashboard data={data} /></Route>
           <Route path="/login"><Login /></Route>
           <Route path="/settings"><Settings /></Route>
           <Redirect to="/dashboard" />
@@ -44,7 +47,12 @@ function App() {
 function MoreInfo(props) {
   const params = useParams();
   const currChoice = params.choice;
-  props.data.filter(data => data.url = currChoice)
+  let data = props.data.filter(data => data.url === currChoice)
+  data = data[0]
+  console.log(data.name)
+  return (
+    <Details url={data.url} name={data.name} image={data.image} description={data.description}/>
+  )
 }
 
 function Dashboard(props) {
---
+++ src/Detail.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+export function Details(props) {
+  console.log(props)
+  let name = props.name;
+  let img = "img/" + props.image;
+  let url = props.url;
+  let desc = props.description;
+  return (
+    <div>
+      <h1>{name}</h1>
+      <img src={img}></img>
+      <p>{desc}</p>
+      <Btn name='Learn about the movement' />
+      <Btn name='Find Resources' />
+      <Btn name='How to get involved' />
+      <Btn name='Opportunities in Seattle' />
+      <Btn name='Current News' />
+      <Btn name='Stories' />
+    </div>
+  )
+}
+
+function Btn(props) {
+  let name = props.name;
+  return (
+    <div>
+      <h2>{name}</h2>
+    </div>
+  )
+}
